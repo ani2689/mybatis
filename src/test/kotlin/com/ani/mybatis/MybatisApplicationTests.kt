c7fb7584370a3fbe7c1ddf8d1defb3b22416c88f
노현주 <dev.hyeonju@gmail.com>
2024-10-19 23:08:44 +0900

    init :: mybatis

---
+++ src/test/kotlin/com/ani/mybatis/MybatisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ani.mybatis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
