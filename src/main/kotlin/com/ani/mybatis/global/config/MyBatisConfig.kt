f1d25983051ed7ce419fd6d381ea13e79a346a2a
노현주 <dev.hyeonju@gmail.com>
2024-10-21 09:49:06 +0900

    add :: sqlSessionFactory 생성

--- src/main/kotlin/com/ani/mybatis/global/config/MyBatisConfig.kt
+++ src/main/kotlin/com/ani/mybatis/global/config/MyBatisConfig.kt
@@ -1,8 +1,13 @@
 package com.ani.mybatis.global.config
 
 import com.ani.mybatis.global.annotation.MyBatisMapper
+import org.apache.ibatis.session.SqlSessionFactory
+import org.mybatis.spring.SqlSessionFactoryBean
 import org.mybatis.spring.annotation.MapperScan
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.apache.ibatis.session.Configuration as SessionConfiguration
 
 @Configuration
 @MapperScan(
@@ -12,4 +17,23 @@ import org.springframework.context.annotation.Configuration
     annotationClass = MyBatisMapper::class
 )
 class MyBatisConfig {
+
+    @Bean
+    fun sqlSessionFactory(): SqlSessionFactory =
+        SqlSessionFactoryBean().apply {
+            val dataSource = DataSourceBuilder
+                .create()
+                .url("localhost:3306")
+                .username("root")
+                .password("1234")
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .build()
+
+            val configuration = SessionConfiguration().apply {
+                isMapUnderscoreToCamelCase = true
+            }
+
+            setDataSource(dataSource)
+            setConfiguration(configuration)
+        }.`object`!!
 }
